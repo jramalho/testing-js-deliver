ad0c84346af7cf2b406db95c01751094abedc298
Jonathan Ramalho <totoramalho@gmail.com>
2023-07-27 15:52:13 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,3 +1,19 @@
 function display() {
    alert("Hello World!");
 }
+
+function handleMessage() {
+   const isIos = [
+    'iPad Simulator',
+    'iPhone Simulator',
+    'iPod Simulator',
+    'iPad',
+    'iPhone',
+    'iPod'
+  ].includes(navigator.platform);
+
+   const msg = JSON.stringify({teste: 'teste'});
+
+   return isIos ? window?.webkit?.messageHandlers?.ReactNativeWebView?.postMessage(msg) : window?.ReactNativeWebView?.postMessage(msg);
+
+}
